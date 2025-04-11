8f668ed9b0fb51525712ae78acc052886493dd7e
Iryna Prus <prusirina12@gmail.com>
2025-04-11 19:30:26 +0300

    update js

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,8 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
-  if (totalPrice > customerCredits) {
-    return "Insufficient funds!";
-  } else {
-    return `You ordered ${quantity} worth ${totalPrice} credits!`;
-  }
+  return totalPrice > customerCredits
+    ? `Insufficient funds!`
+    : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
